5cabd23dba2fe8074c335737939e85fa1ca4230c
crownedjester <kiyoshisatoshy@gmail.com>
2022-08-22 16:58:44 +0300

    Create basic ui template

---
+++ app/src/main/java/com/crownedjester/soft/todosmanager/presentation/AddTodoFragment.kt
@@ -0,0 +1,14 @@
+package com.crownedjester.soft.todosmanager.presentation
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.crownedjester.soft.todosmanager.R
+
+class AddTodoFragment : Fragment(R.layout.fragment_add_todo) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
