fe0518912fbfa0d03521773980d8e2f5692a41ce
crownedjester <kiyoshisatoshy@gmail.com>
2022-08-24 00:40:13 +0300

    change: change invoke method structure

--- app/src/main/java/com/crownedjester/soft/todosmanager/domain/use_cases/get_todos/GetTodos.kt
+++ app/src/main/java/com/crownedjester/soft/todosmanager/domain/use_cases/get_todos/GetTodos.kt
@@ -1,16 +1,11 @@
 package com.crownedjester.soft.todosmanager.domain.use_cases.get_todos
 
 import com.crownedjester.soft.todosmanager.domain.repository.TodosRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 class GetTodos(private val repository: TodosRepository) {
 
-    suspend operator fun invoke(scope: CoroutineScope) =
-        withContext(scope.coroutineContext + Dispatchers.IO) {
-
+    operator fun invoke() =
             repository.getTodos()
-        }
+
 
 }
