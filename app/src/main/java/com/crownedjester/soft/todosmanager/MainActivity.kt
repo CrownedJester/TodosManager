4dd0658436246a178933e511ac8664b877abad61
crownedjester <kiyoshisatoshy@gmail.com>
2022-08-25 02:12:44 +0300

    setup: setup toolbar title according dest and home btn

--- app/src/main/java/com/crownedjester/soft/todosmanager/MainActivity.kt
+++ app/src/main/java/com/crownedjester/soft/todosmanager/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.crownedjester.soft.todosmanager
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val navController by lazy {
+        (supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment).navController
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+
+            supportActionBar?.title =
+                if (destination.id == R.id.dashboardFragment) {
+                    "Todos List"
+                } else {
+                    "Add / Edit Todo"
+                }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return if (item.itemId == android.R.id.home && navController.currentBackStackEntry?.destination?.id == R.id.addTodoFragment) {
+            navController.navigateUp()
+        } else super.onOptionsItemSelected(
+            item
+        )
+
     }
 }
