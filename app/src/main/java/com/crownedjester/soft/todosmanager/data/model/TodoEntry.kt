06e39eb2ea4a5567dfbc7bc41215657c58d48146
crownedjester <kiyoshisatoshy@gmail.com>
2022-08-24 16:34:27 +0300

    change: add and implement kotlin-parcelize plugin

--- app/src/main/java/com/crownedjester/soft/todosmanager/data/model/TodoEntry.kt
+++ app/src/main/java/com/crownedjester/soft/todosmanager/data/model/TodoEntry.kt
@@ -1,8 +1,11 @@
 package com.crownedjester.soft.todosmanager.data.model
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @Entity(tableName = "todo_table")
 data class TodoEntry(
     @PrimaryKey(autoGenerate = true)
@@ -10,4 +13,4 @@ data class TodoEntry(
     val title: String,
     val goal: String,
     val publishDate: String,
-)
+) : Parcelable
