115b83ebc7fab8b5cb31b79ccb1064988d46a44a
crownedjester <kiyoshisatoshy@gmail.com>
2022-08-28 23:29:10 +0300

    add: handle back-pressing

--- app/src/main/java/com/crownedjester/soft/todosmanager/MainActivity.kt
+++ app/src/main/java/com/crownedjester/soft/todosmanager/MainActivity.kt
@@ -2,8 +2,14 @@ package com.crownedjester.soft.todosmanager
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.system.exitProcess
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +17,8 @@ class MainActivity : AppCompatActivity() {
     private val navController by lazy {
         (supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment).navController
     }
+
+    private var countBackPressed = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,12 +39,42 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
-        return if (item.itemId == android.R.id.home && navController.currentBackStackEntry?.destination?.id == R.id.addTodoFragment) {
-            navController.navigateUp()
-        } else super.onOptionsItemSelected(
+        return if (item.itemId == android.R.id.home)
+            navController.navigateIfAvailable()
+        else super.onOptionsItemSelected(
             item
         )
 
     }
+
+    override fun onBackPressed() {
+
+        if (!navController.navigateIfAvailable()) {
+
+            countBackPressed += 1
+
+            lifecycleScope.launch {
+                delay(1250)
+                countBackPressed = 0
+            }
+
+            if (countBackPressed == 1) {
+                Toast.makeText(this, "Tap backpress again to exit", Toast.LENGTH_LONG).show()
+            }
+
+            if (countBackPressed == 2) {
+                finishActivity(0)
+                exitProcess(0)
+            }
+
+        }
+
+    }
+
+    private fun NavController.navigateIfAvailable(): Boolean =
+        if (this.currentBackStackEntry?.destination?.id == R.id.addTodoFragment)
+            this.navigateUp()
+        else
+            false
+
 }
